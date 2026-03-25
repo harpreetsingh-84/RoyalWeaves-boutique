57c8555a8ba497ea41ff33cc97496042f0f99c6e
Harpreet singh <hs271391@gmail.com>
2026-03-25 10:51:44 +0530

    initial commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -56,7 +56,7 @@ mongoose.connect(process.env.MONGO_URI as string)
 })
 .catch(err => console.log(err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
