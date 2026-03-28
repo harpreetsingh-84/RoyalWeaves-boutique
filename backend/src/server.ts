2e7bfff719fb9f9e0d08bc24b88501c7d61f7e34
Harpreet singh <hs271391@gmail.com>
2026-03-28 13:31:37 +0530

    change origin to https://woven-wonder-creation.vercel.app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:5173',
-  process.env.FRONTEND_URL || 'https://royal-weaves-boutique.vercel.app',
+  process.env.FRONTEND_URL ,
    
 ];
 
